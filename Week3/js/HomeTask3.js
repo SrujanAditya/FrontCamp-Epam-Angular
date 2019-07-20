685e879af07325cfed09b1faf0d6c910339fd876
Dasa Srujan Aditya <srujanaditya@gmail.com>
2019-07-20 13:35:26 +0530

    check usage

--- Week3/js/HomeTask3.js
+++ Week3/js/HomeTask3.js
@@ -2,12 +2,13 @@ import { newsApiEndPoint } from "./Endpoints.js";
 import getChannels from "./GetListOfChannels.js";
 
 var channels = getChannels();
-// var data = Promise.resolve(channels).then(result => result);
-// console.log(channels);
 var result;
-console.log(
-  channels.then(data => {
-    result = data;
-  })
-);
-console.log(result);
+function setResult() {
+  console.log("result", result);
+}
+channels.then((val) => {
+  result = val;
+  setResult();
+});
+
+setTimeout(()=>{console.log(result)},2000);
